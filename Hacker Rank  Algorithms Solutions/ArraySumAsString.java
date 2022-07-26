67b8e4a9efdd817feb684006629a034b5c21edb2
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-07-26 11:38:10 +0530

    Create ArraySumAsString.java

---
+++ Hacker Rank  Algorithms Solutions/ArraySumAsString.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+//---------------------------------------------------------------------------------
+
+//Solution Number 1:
+//Solution using for loop
+/*
+public class Solution {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		int sum=0;
+		String[] input = br.readLine().split(" ");
+		for(String value:input)
+		{
+			sum += Integer.parseInt(value);
+		}
+		System.out.println(sum);
+    }
+}*/
+
+
+//---------------------------------------------------------------------------------
+
+//Solution Number 2:
+//Solution using LinkedList
+
+
+
+
+
+
+
