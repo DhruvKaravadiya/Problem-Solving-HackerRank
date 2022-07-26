5f3f1f9504a14757500a2078df8ee3f4daea0f72
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-07-26 11:40:18 +0530

    Update ArraySumAsString.java

--- Hacker Rank  Algorithms Solutions/ArraySumAsString.java
+++ Hacker Rank  Algorithms Solutions/ArraySumAsString.java
@@ -9,6 +9,7 @@ import java.util.regex.*;
 import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
+import java.util.*;
 
 //---------------------------------------------------------------------------------
 
@@ -34,6 +35,41 @@ public class Solution {
 
 //Solution Number 2:
 //Solution using LinkedList
+
+/*
+class Result {
+    public static int simpleArraySum(List<Integer> ar) {   
+        int ans = 0;
+        for(int i = 0; i < ar.size(); i++){
+            ans += ar.get(i);
+        }
+        return ans;
+    }
+}
+
+public class SimpleArraySumUsingList {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+*/
+//---------------------------------------------------------------------
 
 
 
