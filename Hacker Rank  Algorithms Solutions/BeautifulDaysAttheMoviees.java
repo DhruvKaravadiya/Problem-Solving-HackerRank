d8a1bcea966517e20957e721fb081d06168f6152
Dhruv Karavadiya <94845676+DhruvKaravadiya@users.noreply.github.com>
2022-09-10 21:34:28 +0530

    Create BeautifulDaysAttheMoviees.java

---
+++ Hacker Rank  Algorithms Solutions/BeautifulDaysAttheMoviees.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int beautifulDays(int i, int j, int k) {
+        // Write your code here
+        int count = 0;
+        for (int h = i; h <= j; h++) {
+            String str = String.valueOf(h);
+            String nstr = "";
+            for (int t = 0; t < str.length(); t++) {
+                nstr = str.charAt(t) + nstr;
+            }
+            int nInt = Integer.parseInt(nstr);
+            if (Math.abs(h - nInt) % k == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
